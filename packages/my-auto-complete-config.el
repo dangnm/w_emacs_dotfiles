75da6eb37047a95a4039b4847db345ed725bc434
Dang (Wilber) Nguyen(minhdang.net@gmail.com) <minhdang.net@gmail.com>
2019-01-08 14:14:47 +0700

    remove ac-menu-height 50 which makes emacs slow

--- packages/my-auto-complete-config.el
+++ packages/my-auto-complete-config.el
@@ -14,11 +14,15 @@
                (ac-config-default)
                (setq ac-delay 0.02)
                (setq ac-use-menu-map t)
-               (setq ac-menu-height 50)
                (setq ac-use-quick-help nil) 
                (setq ac-ignore-case nil)
                (setq ac-dwim  t)
                (setq ac-fuzzy-enable t)
+               (setq ac-ignore-case t
+                     ac-use-fuzzy t
+                     ac-auto-start 0
+                     ac-auto-show-menu 0.8
+                     ac-expand-on-auto-complete nil)
                ;;Auto-completion complete key mapping
                (with-eval-after-load "evil"
                                      (define-key evil-insert-state-map (kbd "C-f") 'auto-complete)
