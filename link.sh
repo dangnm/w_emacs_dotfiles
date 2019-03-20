789251767d7900df4f1c9337c9e56ec5385e807c
Dang (Wilber) Nguyen(minhdang.net@gmail.com) <minhdang.net@gmail.com>
2019-03-20 14:49:37 +0700

    rebinding copy file path functions

--- link.sh
+++ link.sh
@@ -7,6 +7,7 @@ then
   unlink ~/.emacs.d/assets
   unlink ~/.emacs.d/core
   unlink ~/.emacs.d/init.el
+  unlink ~/.emacs.d/.lock
   unlink ~/.emacs.d/layers
   unlink ~/.emacs.d/private
   unlink ~/.emacs.d/spacemacs.mk
@@ -16,6 +17,7 @@ else
   ln -sf ~/spacemacs/assets ~/.emacs.d/assets
   ln -sf ~/spacemacs/core ~/.emacs.d/core
   ln -sf ~/spacemacs/init.el ~/.emacs.d/init.el
+  ln -sf ~/spacemacs/.lock ~/.emacs.d/.lock
   ln -sf ~/spacemacs/layers ~/.emacs.d/layers
   ln -sf ~/spacemacs/private ~/.emacs.d/private
   ln -sf ~/spacemacs/spacemacs.mk ~/.emacs.d/spacemacs.mk
